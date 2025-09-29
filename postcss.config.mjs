7be660792778b5994b51e4409a209eaf6d5726e5
latoya947 <latoyabillyels51@gmail.com>
2025-09-29 16:51:52 -0700

    postcss issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
     'postcss-preset-env': {
       stage: 1,
