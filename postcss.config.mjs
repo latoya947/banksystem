497e0962c63e269923d4fa6e7aacaa15967677ea
latoya947 <latoyabillyels51@gmail.com>
2025-09-29 16:44:07 -0700

    update postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,20 +1,16 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import postcssPresetEnv from 'postcss-preset-env'
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-    postcssPresetEnv({
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-preset-env': {
       stage: 1,
       features: {
         'oklab-function': true,
         'color-function': { unresolved: 'warn' },
       },
-    }),
-  ],
+    },
+  },
 }
 
 export default config
